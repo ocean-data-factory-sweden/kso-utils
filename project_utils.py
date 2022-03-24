a8c68a4562668ddb3bd26b56dd1d89b6ee1dc53e
Jurie Germishuys <jurie.germishuys@combine.se>
2022-03-24 13:04:01 +0100

    Update project utils to work for SNIC

--- project_utils.py
+++ project_utils.py
@@ -21,20 +21,22 @@ def find_project(project_name: str = ''):
        project csv path and project name'''
     # Specify the path to the list of projects
     project_path = "../db_starter/projects_list.csv"
+    snic_path = "/cephyr/NOBACKUP/groups/snic2021-6-9/"
         
     # Check path to the list of projects is a csv
     if os.path.exists(project_path) and not project_path.endswith(".csv"):
         logging.error("A csv file was not selected. Please try again.")
+
+    elif os.path.exists(project_path) and os.path.exists(snic_path):
+        project_path = "/cephyr/NOBACKUP/groups/snic2021-6-9/db_starter/project_list.csv"
+        read_file.to_csv(project_path)
         
     # If list of projects doesn't exist retrieve it from github
-    if not os.path.exists(project_path):
+    elif not os.path.exists(project_path):
         github_path = "https://github.com/ocean-data-factory-sweden/koster_data_management/blob/main/db_starter/projects_list.csv?raw=true"
         read_file = pd.read_csv(github_path)
-        try:
-            read_file.to_csv(project_path, index=None)
-        except:
-            read_file.to_csv("/cephyr/NOBACKUP/groups/snic2021-6-9/db_starter/project_list.csv")
-        
+        read_file.to_csv(project_path, index=None)
+
     with open(project_path) as csv:
         reader = DataclassReader(csv, Project)
         for row in reader:
@@ -46,7 +48,9 @@ def add_project(project_info: dict = {}):
     project csv using a project_info dictionary
     '''
     project_path = "../db_starter/projects_list.csv"
-    if not os.path.exists(project_path):
+    snic_path = "/cephyr/NOBACKUP/groups/snic2021-6-9/"
+
+    if not os.path.exists(project_path) and os.path.exists(snic_path):
         project_path = "/cephyr/NOBACKUP/groups/snic2021-6-9/db_starter/project_list.csv"
     with open(project_path, "a") as f:
         project = [Project(*list(project_info.values()))]
