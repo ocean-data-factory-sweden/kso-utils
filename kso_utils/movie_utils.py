be19c27953cc146efb822983f41e88e693f8982b
Victor Anton <victor@wildlife.ai>
2023-09-11 18:45:39 +1200

    fix: removed kso movie formatting (no needed)

--- kso_utils/movie_utils.py
+++ kso_utils/movie_utils.py
@@ -720,23 +720,6 @@ def check_movies_meta(
             f" that are not in the movies.csv. Their paths are: {no_info_movies_df.fpath.unique()}"
         )
 
-    if project.server == ServerType.SNIC:
-        # Find closest matching filename (may differ due to Swedish character encoding)
-        parsed_url = urllib.parse.urlparse(movies_df["fpath"].iloc[0])
-
-        def get_match(string, string_options):
-            normalized_string = unicodedata.normalize("NFC", string)
-            for s in string_options:
-                normalized_s = unicodedata.normalize("NFC", s)
-                if normalized_string in normalized_s:
-                    return s
-            return None
-
-        df["fpath"] = df["fpath"].apply(
-            lambda x: get_match(x, available_movies_df["fpath"].unique()),
-            1,
-        )
-
     # Add information about whether the movies are available in the movie_folder
     df_toreview = df.merge(
         available_movies_df[["fpath", "exists"]],
